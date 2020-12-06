0fdd1aeff51e986e2fbde57546c75f3f613dfdda
dvranga <dvranga@users.noreply.github.com>
2020-12-06 16:28:16 +0530

    [ranganath]Refactor:Added addition fields to employee payroll as required and validation on the setter method

--- js/payroll_form.js
+++ js/payroll_form.js
@@ -20,4 +20,83 @@ window.addEventListener('DOMContentLoaded',(event)=>{
     salary.addEventListener('input',function(){
     output.textContent=salary.value;
     })
-});
+
+    const day=document.querySelector('#day');
+    const month=document.querySelector('#month');
+    const year=document.querySelector('#year');
+    const startdateError=document.querySelector('.startdate-error');
+    day.addEventListener('input',function(){
+        try{
+            (new EmployeePayrollData()).startDate=day.value+"-"+month.value+"-"+year.value;;
+            startdateError.textContent="";
+        }catch(e){
+            startdateError.textContent=e;
+        }
+    })
+    month.addEventListener('input',function(){
+        try{
+            (new EmployeePayrollData()).startDate=day.value+"-"+month.value+"-"+year.value;;
+            startdateError.textContent="";
+        }catch(e){
+            startdateError.textContent=e;
+        }
+    })
+    year.addEventListener('input',function(){
+        try{
+            (new EmployeePayrollData()).startDate=day.value+"-"+month.value+"-"+year.value;;
+            startdateError.textContent="";
+        }catch(e){
+            startdateError.textContent=e;
+        }
+    })
+
+})
+
+
+const save=()=>{
+    try {
+        let EmployeePayrollData=createEmployeePayroll();
+    } catch (error) {
+        return;
+    }
+}
+
+const createEmployeePayroll=()=>{
+    let employeePayrollData=new EmployeePayrollData();
+    try {
+        employeePayrollData.name=getInputValueById('#name');
+    } catch (error) {
+        setTextValue('.text-error',e);
+        throw e;
+    }
+
+    employeePayrollData.profilePic=getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender=getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department=getSelectedValues('[name=department]');
+    employeePayrollData.salary=getSelectedValues('#salary');
+    employeePayrollData.note=getInputValueById('#notes');
+    let date=getInputValueById('#day')+" "+getInputValueById('#month')+" "+
+            getInputValueById('#year');
+    employeePayrollData.date=Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getSelectedValues=(propertyValue)=>{
+    let allItmes=document.querySelectorAll(propertyValue);
+    let selItems=[];
+    allItmes.forEach(item =>{
+        if(item.checked) selItems.push(item.value);
+    });
+    return selItems;
+}
+
+const getInputValueById=(id)=>{
+    let value=document.querySelector(id).value;
+    return value;
+}    
+
+const getInputElementValue=(id)=>{
+    let value=document.getElementById(id).value;
+    return value;
+}
